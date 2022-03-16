ac54214fded1f70dbf864ebbb445d32bb1b579d2
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-03-16 14:41:49 +0100

    update some properties in index.php

--- public/src/_header_nav.php
+++ public/src/_header_nav.php
@@ -1,13 +1,13 @@
-        <nav class="headerNavMin">
+        <nav class="headerNavMin headerNavMax">
             <div class="headerLogo">
                 <img src="https://fakeimg.pl/150x50/">
                 <!-- <img src="src/assets/logo/sanstitre.png" alt="logo We love Lyon">-->
             </div>
             <ul class="headerList">
-                <li><a href="/src/index.php">Accueil</a></li>
-                <li><a href="/src/beautiful_sites.php">Sites mémorables</a></li>
-                <li><a href="/src/hotel_restaurants.php">Hôtels & Gastro</a></li>
-                <li><a href="/src/lyon_ballad.php">Ballades</a></li>
-                <li><a href="/src/contact.php">Contact</a></li>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="src/beautiful_sites.php">Sites mémorables</a></li>
+                <li><a href="src/hotel_restaurants.php">Hôtels & Gastro</a></li>
+                <li><a href="src/lyon_ballad.php">Ballades</a></li>
+                <li><a href="src/contact.php">Contact</a></li>
             </ul>
         </nav> 

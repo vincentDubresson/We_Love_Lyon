faedf27b8c5781e5b0e68c4c1255c3e3167f8e72
Cid-Jie <pcidjie@gmail.com>
2022-03-16 16:35:32 +0100

    Add h1 in css init and layout of my page beautiful monuments

--- public/src/beautiful_sites.php
+++ public/src/beautiful_sites.php
@@ -12,6 +12,7 @@
     <meta name="keywords" content="lyon, fourvière, confluence, restaurant, hôtels, monument, musée, parc, ballade, visite, indispensable, wild, code, school">
     <link rel="stylesheet" href="./css/css_init.css">
     <link rel="stylesheet" href="./css/_head_foot_style.css">
+    <link rel="stylesheet" href="./css/beautiful_sites.css">
     <title><!-- Titre à changer --></title>
 </head>
 
@@ -20,13 +21,20 @@
     <header>
         
         <?php include("./_header_nav.php"); ?>
+        <div class="header-type">
+            <p> Le saviez -vous ? </br>
+            Lorsque l'on voit le Mont Blanc, dans 2 jours il pleut !
+        </div> 
+
+</div>
 
     </header>
 
     <div class="photos">
+            <h1> Sites mémorables</h1>
 
             <div class="photo_description">
-                <img src="https://via.placeholder.com/300x350"/>
+                <img class="img" src="https://via.placeholder.com/200x250"/>
                 <p class="title_photo">Titre photo</p>
                 <p>
                 Le Lorem Ipsum est simplement du faux texte employé dans la
@@ -38,7 +46,7 @@
             </div>
 
             <div class="photo_description">
-                <img src="https://via.placeholder.com/300x350"/>
+                <img class="img" src="https://via.placeholder.com/200x250"/>
                 <p class="title_photo">Titre photo</p>
                 <p>
                 Le Lorem Ipsum est simplement du faux texte employé dans la
@@ -50,7 +58,19 @@
             </div>
 
             <div class="photo_description">
-                <img src="https://via.placeholder.com/300x350"/>
+                <img class="img" src="https://via.placeholder.com/200x250"/>
+                <p class="title_photo">Titre photo</p>
+                <p class="text-description>
+                Le Lorem Ipsum est simplement du faux texte employé dans la
+                composition et la mise en page avant impression. Le Lorem Ipsum est
+                le faux texte standard de l'imprimerie depuis les années 1500, quand
+                un imprimeur anonyme assembla ensemble des morceaux de texte pour
+                réaliser un livre spécimen de polices de texte.
+                </p>
+            </div>
+
+            <div class="photo_description">
+                <img class="img" src="https://via.placeholder.com/200x250"/>
                 <p class="title_photo">Titre photo</p>
                 <p>
                 Le Lorem Ipsum est simplement du faux texte employé dans la
@@ -62,19 +82,7 @@
             </div>
 
             <div class="photo_description">
-                <img src="https://via.placeholder.com/300x350"/>
-                <p class="title_photo">Titre photo</p>
-                <p>
-                Le Lorem Ipsum est simplement du faux texte employé dans la
-                composition et la mise en page avant impression. Le Lorem Ipsum est
-                le faux texte standard de l'imprimerie depuis les années 1500, quand
-                un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                réaliser un livre spécimen de polices de texte.
-                </p>
-            </div>
-
-            <div class="photo_description">
-                <img src="https://via.placeholder.com/300x350"/>
+                <img class="img" src="https://via.placeholder.com/200x250"/>
                 <p class="title_photo">Titre photo</p>
                 <p>
                 Le Lorem Ipsum est simplement du faux texte employé dans la
@@ -86,7 +94,7 @@
             </div> 
 
             <div class="photo_description">
-                <img src="https://via.placeholder.com/300x350"/>
+                <img class="img" src="https://via.placeholder.com/200x250"/>
                 <p class="title_photo">Titre photo</p>
                 <p>
                 Le Lorem Ipsum est simplement du faux texte employé dans la

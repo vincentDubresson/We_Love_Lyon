d90cbf450f56a13fb3072945fffe8b85e24c42f7
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-03-16 11:38:59 +0100

    add more text in the footer and update the footer style

--- public/src/_footer.php
+++ public/src/_footer.php
@@ -1,5 +1,8 @@
 <footer>
 
-    <h3>&copy; 2022 We Love Lyon</h3>
+    <div class="text_footer">
+        <h3>&copy; 2022 We Love Lyon</h3>
+        <p>Toutes les images sont libres de droits.</p>
+    </div>
 
 </footer>

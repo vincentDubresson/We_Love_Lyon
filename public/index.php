a54521fd30b5ec6152b095bac8a12d875d7a7fa0
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-03-16 13:34:37 +0100

    Update of midday

--- public/index.php
+++ public/index.php
@@ -25,8 +25,8 @@
     </header>
 
 
-    <!-- place pour le _footer.php -->
-    <?php include('./src/_footer.php'); ?>
+    
+    <!--<?php include('./src/_footer.php'); ?>-->
 
 
     <!-- À confirmer mais n'insérer le script uniquement si besoin pour éviter les erreurs -->
--- public/src/_header_nav.php
+++ public/src/_header_nav.php
@@ -1,13 +1,13 @@
-        <nav class="headerNav">
-            <div class="headerNav">
-                <!--<img src="https://fakeimg.pl/150x50/">-->
-                <img src="src/assets/logo/sanstitre.png" alt="logo We love Lyon">
+        <nav class="headerNavMin">
+            <div class="headerLogo">
+                <img src="https://fakeimg.pl/150x50/">
+                <!-- <img src="src/assets/logo/sanstitre.png" alt="logo We love Lyon">-->
             </div>
-            <ul>
+            <ul class="headerList">
                 <li><a href="/src/index.php">Accueil</a></li>
                 <li><a href="/src/beautiful_sites.php">Sites mémorables</a></li>
                 <li><a href="/src/hotel_restaurants.php">Hôtels & Gastro</a></li>
                 <li><a href="/src/lyon_ballad.php">Ballades</a></li>
                 <li><a href="/src/contact.php">Contact</a></li>
             </ul>
-        </nav>
+        </nav> 

ce16e0084d6fbc68c4dc31aa93eb92f3da182295
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-03-17 19:07:29 +0100

    Resolving problem about double l in ballade

--- public/src/_header_nav.php
+++ public/src/_header_nav.php
@@ -7,7 +7,7 @@
                 <li class="headerNavLinks"><a href="index.php">Accueil</a></li>
                 <li class="headerNavLinks"><a href="src/beautiful_sites.php">Sites mémorables</a></li>
                 <li class="headerNavLinks"><a href="src/hotels_restaurants.php">Hôtels & Gastro</a></li>
-                <li class="headerNavLinks"><a href="src/lyon_ballad.php">Ballades</a></li>
+                <li class="headerNavLinks"><a href="src/lyon_ballad.php">Balades</a></li>
                 <li class="headerNavLinks"><a href="src/contact.php">Contact</a></li>
             </ul>
             <button class="headerNavButton"><img src="src/assets/logo/icons8-chevron-bas-24.png"></button>

81d4cfd1e4051dae9427eb353575e1117cb44db3
Theo_Boucher38 <theo.langlais@hotmail.fr>
2022-03-17 11:30:56 +0100

    remade contact form

--- public/src/contact.php
+++ public/src/contact.php
@@ -10,34 +10,50 @@
     <meta name="description" content="Bienvenue sur We love Lyon ! Découvrez la capitale des Gaules, la seule ville des Lumières.
     Retrouvez les plus beaux sites, les meilleurs hôtels et restaurants et bien plus !">
     <meta name="keywords" content="lyon, fourvière, confluence, restaurant, hôtels, monument, musée, parc, ballade, visite, indispensable, wild, code, school">
-    <link rel="stylesheet" href="css/_head_foot_style.css">
-    <link rel="stylesheet" href="css/css_init.css">
-    <link rel="stylesheet" href="css/_contact">
-    <title>Contact - We Love Lyon </title>
+    <link rel="stylesheet" href="./css/hotels_restaurants.css">
+    <link rel="stylesheet" href="./css/_contact.css">
+    <link rel="stylesheet" href="./css/_head_foot_style.css">
+    <link rel="stylesheet" href="./css/css_init.css">
+    <title>Les hôtels et restaurants incontournables de Lyon</title>
 </head>
 
 <body>
 
     <header>
+        <!-- place pour le _header_nav.php -->
+        <?php include "./_header_nav.php";?>
 
-        <?php require '_header_nav.php'?>
+        <div class="photos">
 
+            <div class="header-type">
+                <p>On mange quoi ?</p>
+                <p>Du jésus, des bugnes, de la rosette, des coussins, des quenelles, de la cervelle de canut ..? </p>
+                <p>Et si on se faisait un petit Bouchon ?</p>
+            </div>
+
+        </div>
     </header>
 
-    <div class="container">
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-  <div class="box"></div>
-</div>
-<form>
-    <h2>Nous contacter</h2>
-    <input type="email" placeholder="@">
-    <input type="text" placeholder="Prénom Nom">
-    <input type="text" placeholder="Votre message">
-</form>
+    <main>
+        <section class="introduction">
+            <h1>Contacter l'équipe de We Love Lyon</h1>
+        </section>
 
-   <footer> <?php require '_footer.php'?> </footer>
+        <section class="formulaire">
+        <form>
+    <h2>Nous contacter</h2>
+    <input type="text" placeholder="@" required="required">
+    <input type="text" placeholder="Prénom Nom" required="required">
+    <input type="text" placeholder="Votre message" required="required">
+    <input class ="submitbutton" type="submit" required="required">
+</form>
+        </section>
+    </main>
+
+    <footer>
+        <!-- place pour le _footer.php -->
+        <?php include './_footer.php';?>
+    </footer>
 
     <!-- À confirmer mais n'insérer le script uniquement si besoin pour éviter les erreurs -->
 </body>

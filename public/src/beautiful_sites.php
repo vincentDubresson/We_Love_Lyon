82e63f59ee3cd2e3f40e1a9d47358df85e99ad33
Cid-Jie <pcidjie@gmail.com>
2022-03-17 09:17:08 +0100

    add layout of my page in responsive

--- public/src/beautiful_sites.php
+++ public/src/beautiful_sites.php
@@ -10,10 +10,10 @@
     <meta name="description" content="Bienvenue sur We love Lyon ! Découvrez la capitale des Gaules, la seule ville des Lumières.
     Retrouvez les plus beaux sites, les meilleurs hôtels et restaurants et bien plus !">
     <meta name="keywords" content="lyon, fourvière, confluence, restaurant, hôtels, monument, musée, parc, ballade, visite, indispensable, wild, code, school">
-    <link rel="stylesheet" href="./css/css_init.css">
-    <link rel="stylesheet" href="./css/_head_foot_style.css">
-    <link rel="stylesheet" href="./css/beautiful_sites.css">
-    <title><!-- Titre à changer --></title>
+    <link rel="stylesheet" href="css/css_init.css">
+    <link rel="stylesheet" href="css/_head_foot_style.css">
+    <link rel="stylesheet" href="css/beautiful_sites.css">
+    <title>Les sites mémorables - We Love Lyon</title>
 </head>
 
 <body>
@@ -21,91 +21,84 @@
     <header>
         
         <?php include("./_header_nav.php"); ?>
-        
+        <div class="header_type">
+                <p> Le saviez -vous ? </br>
+                 Lorsque l'on voit le Mont Blanc, dans 2 jours il pleut !
+        </div>
 
     </header>
 
+    <main>
+
     <div class="photos">
 
-            <div class="header-type">
-                <p> Le saviez -vous ? </br>
-                 Lorsque l'on voit le Mont Blanc, dans 2 jours il pleut !
-            </div> 
+      
             
-            <h1> Sites mémorables</h1>
+        <h1> Sites mémorables</h1>
 
+        <div class="parent_description">    
             <div class="photo_description">
-                <img class="img" src="https://via.placeholder.com/200x250"/>
-                <p class="title_photo">Titre photo</p>
-                <p>
-                Le Lorem Ipsum est simplement du faux texte employé dans la
-                composition et la mise en page avant impression. Le Lorem Ipsum est
-                le faux texte standard de l'imprimerie depuis les années 1500, quand
-                un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                réaliser un livre spécimen de polices de texte.
+                <img class="img" src="assets/picture/grande_roue.jpg" alt="place_bellecour"/>
+                <div class="photo_text">
+                <p class="title_photo">La place Bellecour</p>
+                <p class="content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
                 </p>
+                </div>
             </div>
 
             <div class="photo_description">
-                <img class="img" src="https://via.placeholder.com/200x250"/>
-                <p class="title_photo">Titre photo</p>
-                <p>
-                Le Lorem Ipsum est simplement du faux texte employé dans la
-                composition et la mise en page avant impression. Le Lorem Ipsum est
-                le faux texte standard de l'imprimerie depuis les années 1500, quand
-                un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                réaliser un livre spécimen de polices de texte.
+                <img class="img" src="assets/picture/musée_confluence.jpg" alt="musée_confluence"/>
+                <div class="photo_text">
+                <p class="title_photo">Le musée des confluences</p>
+                <p class="content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
                 </p>
-            </div>
-
-            <div class="photo_description">
-                <img class="img" src="https://via.placeholder.com/200x250"/>
-                <p class="title_photo">Titre photo</p>
-                <p class="text-description>
-                Le Lorem Ipsum est simplement du faux texte employé dans la
-                composition et la mise en page avant impression. Le Lorem Ipsum est
-                le faux texte standard de l'imprimerie depuis les années 1500, quand
-                un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                réaliser un livre spécimen de polices de texte.
-                </p>
-            </div>
-
-            <div class="photo_description">
-                <img class="img" src="https://via.placeholder.com/200x250"/>
-                <p class="title_photo">Titre photo</p>
-                <p>
-                Le Lorem Ipsum est simplement du faux texte employé dans la
-                composition et la mise en page avant impression. Le Lorem Ipsum est
-                le faux texte standard de l'imprimerie depuis les années 1500, quand
-                un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                réaliser un livre spécimen de polices de texte.
-                </p>
-            </div>
-
-            <div class="photo_description">
-                <img class="img" src="https://via.placeholder.com/200x250"/>
-                <p class="title_photo">Titre photo</p>
-                <p>
-                Le Lorem Ipsum est simplement du faux texte employé dans la
-                composition et la mise en page avant impression. Le Lorem Ipsum est
-                le faux texte standard de l'imprimerie depuis les années 1500, quand
-                un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                réaliser un livre spécimen de polices de texte.
-                </p>
-            </div> 
-
-            <div class="photo_description">
-                <img class="img" src="https://via.placeholder.com/200x250"/>
-                <p class="title_photo">Titre photo</p>
-                <p>
-                Le Lorem Ipsum est simplement du faux texte employé dans la
-                composition et la mise en page avant impression. Le Lorem Ipsum est
-                le faux texte standard de l'imprimerie depuis les années 1500, quand
-                un imprimeur anonyme assembla ensemble des morceaux de texte pour
-                réaliser un livre spécimen de polices de texte.
-                </p>
+                </div>
             </div>   
 
+            <div class="photo_description">
+                <img class="img" src="assets/picture/parc.jpg" alt="place_tete_dor"/>
+                <div class="photo_text">
+                <p class="title_photo">Le parc de la tête d'or</p>
+                <p class="content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                </p>
+                </div>
+            </div>
+
+            <div class="photo_description">
+                <img class="img" src="assets/picture/amphitheatre.jpg" alt="amphitheatre"/>
+                <div class="photo_text">
+                <p class="title_photo">L'amphithéâtre gallo-romain</p>
+                <p class="content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                </p>
+                </div>
+            </div>
+
+            <div class="photo_description">
+                <img class="img" src="assets/picture/cathedrale.jpg" alt="cathedrale"/>
+                <div class="photo_text">
+                <p class="title_photo">La cathédrale Saint-Jean</p>
+                <p class="content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                </p>
+                </div>
+            </div>
+
+            <div class="photo_description">
+                <img class="img" src="assets/picture/fourviere.jpg" alt="fourviere"/>
+                <div class="photo_text">
+                <p class="title_photo">Fourvière</p>
+                <p class="content">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                </p>
+                </div>
+            </div> 
+
+        </div>
+    </main>
     <footer>
 
         <?php include('./_footer.php'); ?>

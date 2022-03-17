185c8f561db9e1f255839ab2888db1655cf1730e
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-03-17 12:20:42 +0100

    add margin on main in css_init.css

--- public/src/lyon_ballad.php
+++ public/src/lyon_ballad.php
@@ -19,13 +19,46 @@
 <body>
 
     <header>
+
         <?php include("_header_nav.php") ?>
-        <!-- place pour le _header_nav.php -->
+        
+        <div class="headerTitle">
+            <p>Le saviez-vous : Si vous voyez le Mont Blanc depuis ce point de vue, il pleut deux jours plus tard !</p>
+        </div>
 
     </header>
     <main>
-        <iframe src="https://www.google.com/maps/d/embed?mid=1k6HnWGIYafP2l3RmQTJobhyy5RTvUOwR&ehbc=2E312F" width="350" height="350"></iframe>
-        <iframe src="https://www.google.com/maps/d/embed?mid=1P0iR-xrCRzxUj_3u8U30WID8TiySdFzD&ehbc=2E312F" width="350" height="350"></iframe>
+
+        <section class="introduction">
+            <h1>Ballade sur 1 ou 2 jours</h1>
+            <p>Même si Lyon est une grande ville, elle n'en reste pas moins à taille humaine. Vous pouvez tout faire à pied, même si attention : quelques fois, ça grimpe !</p>
+            <p>Nous vous proposons deux parcours qui vous permettrons de visiter Lyon, sur un ou deux jours.</p>
+            <p>Bonne ballade !</p>
+        </section>
+
+        <article>
+            <h2>Première journée</h2>
+            <iframe src="https://www.google.com/maps/d/embed?mid=1k6HnWGIYafP2l3RmQTJobhyy5RTvUOwR&ehbc=2E312F" width="350" height="350"></iframe>
+            <div class="journey">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+            </div>
+        </article>
+
+        <article>
+            <h2>Deuxième journée</h2>
+            <iframe src="https://www.google.com/maps/d/embed?mid=1P0iR-xrCRzxUj_3u8U30WID8TiySdFzD&ehbc=2E312F" width="350" height="350"></iframe>
+            <div class="journey">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet perferendis nesciunt necessitatibus, molestias doloremque reprehenderit aliquam voluptate nulla repellat delectus architecto modi ut non, alias quasi vero perspiciatis natus sit.</p>
+            </div>
+        </article>
     </main>
     <footer>
         <?php include("_footer.php") ?>

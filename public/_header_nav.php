bf9d3303caa468ddb34506be69142cbdb9769cf4
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-03-18 10:43:53 +0100

    create pages hotels and pages restaurants and deleted page hotels_restaurants

--- public/_header_nav.php
+++ public/_header_nav.php
@@ -6,7 +6,8 @@
             <ul class="headerNavList">
                 <li class="headerNavLinks"><a href="index.php">Accueil</a></li>
                 <li class="headerNavLinks"><a href="beautiful_sites.php">Sites mémorables</a></li>
-                <li class="headerNavLinks"><a href="hotels_restaurants.php">Hôtels & Gastro</a></li>
+                <li class="headerNavLinks"><a href="hotels.php">Hôtels</a></li>
+                <li class="headerNavLinks"><a href="restaurants.php">Restaurants</a></li>
                 <li class="headerNavLinks"><a href="lyon_ballad.php">Balades</a></li>
                 <li class="headerNavLinks"><a href="contact.php">Contact</a></li>
             </ul>

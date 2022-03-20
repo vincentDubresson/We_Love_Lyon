e3d325ab74a8f677189f9c95a65cc86aecb358cd
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-03-20 10:37:36 +0100

    adding content to the restaurants file

--- public/_restaurants_array.php
+++ public/_restaurants_array.php
@@ -20,7 +20,35 @@ $compagnysInformations = [
         'compagnyPostalAddress' => ['7 Rue de la Martinière, 69001 Lyon'],
         'compagnyGoogleMapsUrl' => ['https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11132.580149443595!2d4.8289931!3d45.7682835!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa58c48a953f25b38!2zU2FwbsOg!5e0!3m2!1sfr!2sfr!4v1647720741085!5m2!1sfr!2sfr'],
         'compagnyImageLink' => ['./src/assets/picture/restaurant_sapna_lyon_.webp']
+    ],
+    [
+        'compagnyCompagnyName' => ['Le petit Bouclard'],
+        'compagnyBookingUrl' => ['https://fr-fr.facebook.com/petitbouclard/'],
+        'compagnyCallingNumber' => ['+33472943363'],
+        'compagnyWebSiteUrl' => ['https://fr-fr.facebook.com/petitbouclard/'],
+        'compagnyPostalAddress' => ['5 Rue des Trois Pierres, 69007 Lyon'],
+        'compagnyGoogleMapsUrl' => ['https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11136.406751971052!2d4.8451363!3d45.7491073!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc5cc6dc858bd7618!2sLe%20petit%20Bouclard!5e0!3m2!1sfr!2sfr!4v1647767846702!5m2!1sfr!2sfr'],
+        'compagnyImageLink' => ['./src/assets/picture/ptibouclard.jpg']
+    ],
+    [
+        'compagnyCompagnyName' => ['La Maison Bleue'],
+        'compagnyBookingUrl' => ['https://bar-lamaisonbleue.com/'],
+        'compagnyCallingNumber' => ['+33767012247'],
+        'compagnyWebSiteUrl' => ['https://bar-lamaisonbleue.com/'],
+        'compagnyPostalAddress' => ['24 rue Croix-Barret 69007 Lyon'],
+        'compagnyGoogleMapsUrl' => ['https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11138.899412792416!2d4.8422761!3d45.7366125!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x68e9b0cfb38dc86d!2sLa%20Maison%20Bleue%20-%20Caf%C3%A9%2C%20Restaurant%20et%20Bar%20%C3%A0%20bi%C3%A8res%20locales%20-%20Lyon%207!5e0!3m2!1sfr!2sfr!4v1647768339004!5m2!1sfr!2sfr'],
+        'compagnyImageLink' => ['./src/assets/picture/maisonbleu.jpg']
+    ],
+    [
+        'compagnyCompagnyName' => ['Okawali Croix-Rousse'],
+        'compagnyBookingUrl' => ['https://okawali-croixrousse.jimdosite.com/'],
+        'compagnyCallingNumber' => ['+33472070699'],
+        'compagnyWebSiteUrl' => ['https://okawali-croixrousse.jimdosite.com/'],
+        'compagnyPostalAddress' => ['7 Rue Dumenge, 69004 Lyon'],
+        'compagnyGoogleMapsUrl' => ['https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11130.880804569688!2d4.8338784!3d45.7767974!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7271c5d325c19210!2sOkawali%20Croix-Rousse!5e0!3m2!1sfr!2sfr!4v1647768760244!5m2!1sfr!2sfr'],
+        'compagnyImageLink' => ['./src/assets/picture/okawali.jpeg']
     ]
 ];
 
 ?>
+
